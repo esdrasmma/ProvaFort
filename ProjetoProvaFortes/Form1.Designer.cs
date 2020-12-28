f04320882c99551742662be7a031256fb4dbb322
Marcelo Esdras <esdrasmma@gmail.com>
2020-12-27 21:07:38 -0300

    Projeto lendo txt inserindo no banco, e lendo banco e fazendo txt com logica json

---
+++ ProjetoProvaFortes/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace ProjetoProvaFortes
+{
+    partial class btnJson
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeitorTxt = new System.Windows.Forms.Button();
+            this.btnJason = new System.Windows.Forms.Button();
+            this.btnXML = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLeitorTxt
+            // 
+            this.btnLeitorTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeitorTxt.Location = new System.Drawing.Point(52, 87);
+            this.btnLeitorTxt.Name = "btnLeitorTxt";
+            this.btnLeitorTxt.Size = new System.Drawing.Size(180, 140);
+            this.btnLeitorTxt.TabIndex = 0;
+            this.btnLeitorTxt.Text = "Leitor de .Txt";
+            this.btnLeitorTxt.UseVisualStyleBackColor = true;
+            this.btnLeitorTxt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnJason
+            // 
+            this.btnJason.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJason.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnJason.Location = new System.Drawing.Point(300, 87);
+            this.btnJason.Name = "btnJason";
+            this.btnJason.Size = new System.Drawing.Size(180, 140);
+            this.btnJason.TabIndex = 1;
+            this.btnJason.Text = "Gerador  .Json";
+            this.btnJason.UseVisualStyleBackColor = true;
+            this.btnJason.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnXML
+            // 
+            this.btnXML.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXML.Location = new System.Drawing.Point(548, 87);
+            this.btnXML.Name = "btnXML";
+            this.btnXML.Size = new System.Drawing.Size(180, 140);
+            this.btnXML.TabIndex = 2;
+            this.btnXML.Text = "Gerador .XML";
+            this.btnXML.UseVisualStyleBackColor = true;
+            this.btnXML.Click += new System.EventHandler(this.btnXML_Click);
+            // 
+            // btnJson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 306);
+            this.Controls.Add(this.btnXML);
+            this.Controls.Add(this.btnJason);
+            this.Controls.Add(this.btnLeitorTxt);
+            this.Name = "btnJson";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela Principal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeitorTxt;
+        private System.Windows.Forms.Button btnJason;
+        private System.Windows.Forms.Button btnXML;
+    }
+}
+
---
+++ ProjetoProvaFortes/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProjetoProvaFortes
+{
+    public partial class btnJson : Form
+    {
+        DBProvaFortesEntities db = new DBProvaFortesEntities();
+        public int itensPersistindos = 0;
+
+        public btnJson()
+        {
+            InitializeComponent();
+        }
+
+        public void LerTxt()
+        {
+            string path = "NFSe.txt";
+            
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        
+                        string[] fields = sr.ReadLine().Split('|');
+                        string code = fields[0];
+
+                        
+                        if (fields[0] == "P")
+                        {
+                            tbPrestador prestador = new tbPrestador(
+                                fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8], fields[9]);
+
+
+
+                            db.tbPrestador.Add(prestador);
+
+
+                            db.SaveChanges();
+
+                            itensPersistindos++;
+
+                        }
+
+                        if (fields[0] == "T")
+                        {
+                            tbTomador tomador = new tbTomador(fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8]);
+
+                            db.tbTomador.Add(tomador);
+                            db.SaveChanges();
+                            itensPersistindos++;
+
+
+                        }
+                        if (fields[0] == "R")
+                        {
+                            tbNota nota = new tbNota(fields[1], fields[2],fields[3]);
+                            db.tbNota.Add(nota);
+                            db.SaveChanges();
+                            itensPersistindos++;
+
+                        }
+
+                        if (fields[0] == "K")
+                        {
+                            tbItem itens = new tbItem(fields[1], fields[2], fields[3], fields[4], fields[5]);
+                            db.tbItem.Add(itens);
+                            db.SaveChanges();
+                            itensPersistindos++;
+
+
+
+                        }
+                        
+                    }
+                }
+                MessageBox.Show("Total de Itens Persistidos :" + itensPersistindos);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro");
+                MessageBox.Show(e.Message);
+            }
+        }
+
+
+        public void CriarJson()
+        {
+            string pathJson = "JSON.txt";
+            using (var repo = new DBProvaFortesEntities())
+            {
+                IList<tbPrestador> prestador = repo.tbPrestador.ToList();
+                IList<tbTomador> tomador = repo.tbTomador.ToList();
+                IList<tbNota> nota = repo.tbNota.ToList();
+                IList<tbItem> item = repo.tbItem.ToList();
+
+                foreach (var p in prestador)
+                {
+                    string x = "";
+                    x = "Prestador   {" + Environment.NewLine 
+
+                        + "\"CNPJ\" : \"" + p.CNPJ + "\"," + Environment.NewLine 
+                        + "\"Nome\" : \"" + p.NOME + "\"," + Environment.NewLine 
+                        + "\"RazaoSocial\" : \"" + p.RAZAOSOCIAL + "\"," + Environment.NewLine 
+                        + "\"Rua\" : \"" + p.RUA + "\"," + Environment.NewLine 
+                        + "\"Numero\" : \"" + p.NUMERO + "\"," + Environment.NewLine
+                        + "\"Bairro\" : \"" + p.BAIRRO + "\"," + Environment.NewLine
+                        + "\"UF\": \"" + p.UF + "\"," + Environment.NewLine
+                        + "\"CEP\" : \"" + p.CEP + "\"," + Environment.NewLine
+                        + "\"Telefone\" : \"" + p.TELEFONE + "\"," + Environment.NewLine + "}";
+
+                    using (StreamWriter sw = File.AppendText(pathJson))
+                    {
+                        sw.WriteLine(x);
+                    }
+                   
+
+                }
+
+                foreach (var t in tomador)
+                {
+                    string x = "";
+                    x = "Tomador   {" + Environment.NewLine
+                        + "\"CNPJ\" : \"" + t.CNPJ+ "\"," + Environment.NewLine
+                        + "\"Nome\" : \"" + t.NOME + "\"," + Environment.NewLine
+                        + "\"Rua\" : \"" + t.RUA + "\"," + Environment.NewLine
+                        + "\"Numero\" : \"" + t.NUMERO + "\"," + Environment.NewLine
+                        + "\"Bairro\" : \"" + t.BAIRRO + "\"," + Environment.NewLine
+                        + "\"UF\": \"" + t.UF + "\"," + Environment.NewLine
+                        + "\"Telefone\" : \"" + t.TELEFONE + "\"," + Environment.NewLine 
+                        +"\"Email\" : \"" + t.EMAIL + "\"," + Environment.NewLine + "}";
+                    using (StreamWriter sw = File.AppendText(pathJson))
+                    {
+                        sw.WriteLine(x);
+                    }
+
+
+                }
+
+                foreach (var n in nota)
+                {
+                    string x = "";
+                    x = "Nota   {" + Environment.NewLine
+                        + "\"Numero\" : \"" + n.NUMERO + "\"," + Environment.NewLine
+                        + "\"Data\" : \"" + n.DATA + "\"," + Environment.NewLine
+                         + "\"Valor\" : \"" + n.VALOR + "\"," + Environment.NewLine + "}";
+
+                    using (StreamWriter sw = File.AppendText(pathJson))
+                    {
+                        sw.WriteLine(x);
+                    }
+
+
+
+                }
+
+                foreach (var i in item)
+                {
+                    string x = "";
+                    x = "Item   {" + Environment.NewLine
+                         + "\"Matricula\" : \"" + i.MATRICULA + "\"," + Environment.NewLine
+                        + "\"Numero\" : \"" + i.NUMERO + "\"," + Environment.NewLine
+                        + "\"Nome\" : \"" + i.NOME + "\"," + Environment.NewLine
+                        + "\"Quantidade\" : \"" + i.QUANTIDADE + "\"," + Environment.NewLine
+                        + "\"Valor\" : \"" + i.VALOR + "\"," + Environment.NewLine + "}";
+                    ;
+
+                    using (StreamWriter sw = File.AppendText(pathJson))
+                    {
+                        sw.WriteLine(x);
+                    }
+
+                }
+            }
+
+        }
+        public void CriarXML()
+        {
+            string pathJson = "XML.txt";
+            using (var repo = new DBProvaFortesEntities())
+            {
+                IList<tbPrestador> prestador = repo.tbPrestador.ToList();
+                IList<tbTomador> tomador = repo.tbTomador.ToList();
+                IList<tbNota> nota = repo.tbNota.ToList();
+                IList<tbItem> item = repo.tbItem.ToList();
+
+                foreach (var p in prestador)
+                {
+                    string x = "";
+                    x = " CNPJ : " + p.CNPJ
+                        + " Nome : " + p.NOME
+                        + " RazaoSocial : " + p.RAZAOSOCIAL
+                        + " Rua : " + p.RUA
+                        + " Numero : " + p.NUMERO
+                        + " Bairro : " + p.BAIRRO
+                        + " UF : " + p.UF
+                        + " CEP : " + p.CEP
+                        + " Telefone : " + p.TELEFONE;
+
+                    using (StreamWriter sw = File.AppendText(pathJson))
+                    {
+                        sw.WriteLine(x);
+                    }
+
+
+                }
+
+                foreach (var t in tomador)
+                {
+                    string x = "";
+                    x = " CNPJ : " + t.CNPJ
+                        + " Nome : " + t.NOME
+                        + " Rua : " + t.RUA
+                        + " Numero : " + t.NUMERO
+                        + " Bairro : " + t.BAIRRO
+                        + " UF : " + t.UF
+                        + " Telefone : " + t.TELEFONE
+                        + " Email : " + t.EMAIL;
+                    using (StreamWriter sw = File.AppendText(pathJson))
+                    {
+                        sw.WriteLine(x);
+                    }
+
+
+                }
+
+                foreach (var n in nota)
+                {
+                    string x = "";
+                    x = " Numero : " + n.NUMERO
+                        + " Data : " + n.DATA
+                        + " Valor : " + n.VALOR;
+
+                    using (StreamWriter sw = File.AppendText(pathJson))
+                    {
+                        sw.WriteLine(x);
+                    }
+
+
+
+                }
+
+                foreach (var i in item)
+                {
+                    string x = "";
+                    x = " Matricula : " + i.MATRICULA
+                        + " Numero : " + i.NUMERO
+                        + " Nome : " + i.NOME
+                        + " Quantidade : " + i.QUANTIDADE
+                        + " Valor : " + i.VALOR
+                        ;
+
+                    using (StreamWriter sw = File.AppendText(pathJson))
+                    {
+                        sw.WriteLine(x);
+                    }
+
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LerTxt();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CriarJson();
+        }
+
+        private void btnXML_Click(object sender, EventArgs e)
+        {
+            CriarXML();
+        }
+    }
+}
